f1b1020b29eb99c8eea71505bd726fdfb7b434be
Charithma Janani Kariyawasam <charithmajanani@gmail.com>
2026-09-05 12:40:18 +0530

    feat: add cron job for health check and update dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,37 +3,41 @@ import express from 'express';
 import User from './models/user.model.js';
 import { connectDB } from './lib/db.js';
 import "dotenv/config.js";
-import fs from  'fs';
-import {clerkMiddleware} from "@clerk/express";
-import cors from 'cors'; 
+import fs from 'fs';
+import { clerkMiddleware } from "@clerk/express";
+import cors from 'cors';
 import path from 'path';
+import job from './lib/cron.js';
 
 const app = express();
 
 const PORT = process.env.PORT;
-const FRONTEND_URL= process.env.FRONTEND_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const publicDir = path.join(process.cwd(),"public");
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json())
-app.use(cors({origin:FRONTEND_URL, credentials:true})); //allow frontend to access backend
+app.use(cors({ origin: FRONTEND_URL, credentials: true })); //allow frontend to access backend
 app.use(clerkMiddleware());
 
-app.get("/health", (req,res)=>{
-    res.status(200).json({ok: true});
+app.get("/health", (req, res) => {
+    res.status(200).json({ ok: true });
 });
 
-if(fs.existsSync(publicDir)){
+if (fs.existsSync(publicDir)) {
 
-app.use(express.static(publicDir))
+    app.use(express.static(publicDir))
 
-app.get("/{*any}",(req,res,next)=>{
-    res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
-});
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
 
 }
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("Server is up and running on PORT:" , PORT)
+    console.log("Server is up and running on PORT:", PORT)
+
+    if (process.env.NODE_ENV === "production")   job.start();
+    
 });
